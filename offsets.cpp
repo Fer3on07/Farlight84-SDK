8c5cf1b740283d2218e3e95991f56345a6cfeb92
Fer3on <134983326+Fer3on07@users.noreply.github.com>
2023-12-08 12:17:45 +0200

    Update offsets.cpp

--- offsets.cpp
+++ offsets.cpp
@@ -15,9 +15,9 @@
 
 namespace offsets {
     // GAME
-    const std::uint32_t GWorld = 0x6FD55B8;
-    const std::uint32_t GNames = 0x6E5D000;
-    const std::uint32_t GOBJECT = 0x6E75980;
+    const std::uint32_t GWorld = 0x6FD5638;
+    const std::uint32_t GNames = 0x6E5D080;
+    const std::uint32_t GOBJECT = 0x06E75A00;
 
     // Launcher
     const std::uint32_t Launcher_GWorld = 0x6FD45B8;
